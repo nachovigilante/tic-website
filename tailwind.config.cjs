1a76ad396ca98de745752f67034f604037cee25f
nachovigilante <nachovigilante@gmail.com>
2023-03-10 18:17:40 -0300

    build(tailwind): Added basic tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+            mono: ['Fira Code', 'monospace'],
+            space: ['Space Mono', 'monospace'],
+            raleway: ['Raleway', 'sans-serif'],
+        },
+        extend: {
+            borderWidth: {
+                1: '1px',
+                3: '3px',
+            },
+            borderRadius: {
+                default: '15px',
+            },
+            colors: {
+                primary: '#030026',
+                hover: '',
+                'sec-hover': '',
+                shadow: '',
+                accent: '',
+                'accent-sec': '',
+                background: {
+                    dark: '',
+                },
+            },
+        },
+    },
+    plugins: [],
 };
 
 module.exports = config;
