4bd4b4d3ad7be89052b8258f98a08f25120773c4
nachovigilante <nachovigilante@gmail.com>
2023-03-30 00:07:37 -0300

    feat: created login
    Can't know if it's working because of CORS, of cors

--- src/pages/students.tsx
+++ src/pages/students.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import { useQuery } from "react-query";
+import Section from "~/components/utils/Section";
 
 import { Student, useStudents } from "~/hooks/api/useStudents";
 
@@ -20,12 +21,18 @@ const StudentsPage: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="pt-[67px] bg-background-dark">
-                <h2>A</h2>
-                <ul>
-                    {students?.map((student: Student) => (
-                        <li key={student.dni}>{student.name}</li>
-                    ))}
-                </ul>
+                <Section className="min-h-screen">
+                    <h2>A</h2>
+                    {isLoading && <p>Loading...</p>}
+                    {isError && <p>Error</p>}
+                    {!isLoading && !isError && (
+                        <ul>
+                            {students?.map((student: Student) => (
+                                <li key={student.dni}>{student.name}</li>
+                            ))}
+                        </ul>
+                    )}
+                </Section>
             </main>
         </>
     );
