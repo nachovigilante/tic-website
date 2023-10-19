3cb540bcfa3e02af86d11b7dfa7b173c83a2ff2d
nachovigilante <nachovigilante@gmail.com>
2023-10-19 18:37:10 -0300

    refactor: moved masonry to a new file

---
+++ src/components/admin/project/Masonry.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+
+export const Masonry = ({
+    children,
+    cols = 3,
+}: {
+    children: ReactNode;
+    cols?: number;
+}) => {
+    const childrenArray = Array.from(children as Iterable<ReactNode>).flat();
+    console.log(childrenArray);
+
+    return (
+        <div className="grid w-full grid-cols-3 gap-4 max-h-full overflow-y-auto scroll-xs pr-3">
+            {Array.from({ length: cols }).map((_, i) => (
+                <MasonryColumn key={i}>
+                    {childrenArray
+                        .filter((_, idx) => idx % cols === i)
+                        .map((child) => (
+                            <>{child}</>
+                        ))}
+                </MasonryColumn>
+            ))}
+        </div>
+    );
+};
+
+export const MasonryColumn = ({ children }: { children: ReactNode }) => {
+    return <div className="flex gap-4 flex-col justify-start">{children}</div>;
+};
--- src/components/admin/project/ProjectAssignments.tsx
+++ src/components/admin/project/ProjectAssignments.tsx
@@ -1,40 +1,11 @@
 import { Project } from "~/hooks/api/useProjects";
 import Assignemt from "../Assignment";
-import { ReactNode, useRef } from "react";
-
-const MasonryColumn = ({ children }: { children: ReactNode }) => {
-    return <div className="flex gap-4 flex-col justify-start">{children}</div>;
-};
-
-const Masonry = ({
-    children,
-    cols = 3,
-}: {
-    children: ReactNode;
-    cols: number;
-}) => {
-    const childrenArray = Array.from(children as Iterable<ReactNode>).flat();
-    console.log(childrenArray);
-
-    return (
-        <div className="grid w-full grid-cols-3 gap-4 max-h-full overflow-y-auto scroll-xs pr-3">
-            {Array.from({ length: cols }).map((_, i) => (
-                <MasonryColumn key={i}>
-                    {childrenArray
-                        .filter((_, idx) => idx % cols === i)
-                        .map((child) => (
-                            <>{child}</>
-                        ))}
-                </MasonryColumn>
-            ))}
-        </div>
-    );
-};
+import { Masonry } from "./Masonry";
 
 const ProjectAssignemts = ({ project }: { project: Project }) => {
     return (
         <div className="project-card flex-grow rounded-xl pr-4">
-            <Masonry cols={3}>
+            <Masonry>
                 {project.assignments &&
                     project.assignments
                         .sort((a, b) => -1)
